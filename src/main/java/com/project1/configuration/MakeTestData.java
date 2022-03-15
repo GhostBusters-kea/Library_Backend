afe553e9aafc462eaaac8b4cc143b3ef71353d6c
Lars <larsbrogaard86@gmail.com>
2022-03-15 14:36:20 +0100

    Reservation entity and repo plus MakeTestData

--- src/main/java/com/project1/configuration/MakeTestData.java
+++ src/main/java/com/project1/configuration/MakeTestData.java
@@ -1,4 +1,33 @@
 package com.project1.configuration;
 
-public class MakeTestData {
+
+import com.project1.repository.MemberRepository;
+import com.project1.repository.ReservationRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Profile("!test")
+public class MakeTestData implements ApplicationRunner {
+
+    ReservationRepository reservationRepository;
+    MemberRepository memberRepository;
+
+    public MakeTestData(ReservationRepository reservationRepository, MemberRepository memberRepository) {
+        this.reservationRepository = reservationRepository;
+        this.memberRepository = memberRepository;
+    }
+
+    public void makeData(){
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args){
+        memberRepository.deleteAll();
+        makeData();
+    }
+
 }
