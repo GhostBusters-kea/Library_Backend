6ce30f9fe8a5ffd2d23c662721cc6d9a8003b0c5
jmunkc <jona04g8@stud.kea.dk>
2022-03-25 12:21:47 +0100

    HotFix - removed security

--- src/main/java/com/project1/Projekt1Gruppe2Application.java
+++ src/main/java/com/project1/Projekt1Gruppe2Application.java
@@ -1,10 +1,8 @@
 package com.project1;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class Projekt1Gruppe2Application {
---
+++ src/main/java/com/project1/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.project1.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+//@Configuration
+//@EnableWebMvc
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:63343");
+    }
+}
+
